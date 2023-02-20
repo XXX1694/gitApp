4488a9286f81a4d2c2d1ae96162447fbec8d1656
Abzal Serikbay <abzal@Abzals-MacBook-Air.local>
2023-02-20 22:42:52 +0600

    localization was added (without language change function)

--- lib/features/login/presentation/pages/login_page.dart
+++ lib/features/login/presentation/pages/login_page.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(AppLocalizations.of(context)!.test),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:fit_app/features/login/presentation/bloc/login_bloc.dart';
 import 'package:fit_app/features/login/presentation/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+// For building models: flutter pub run build_runner build --delete-conflicting-outputs
 
 void main() async {
   await dotenv.load(fileName: '.env');
@@ -16,8 +20,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
-      providers: const [],
+      providers: [
+        BlocProvider(
+          create: (context) => LoginBloc(),
+        )
+      ],
       child: MaterialApp(
+        localizationsDelegates: AppLocalizations.localizationsDelegates,
+        supportedLocales: AppLocalizations.supportedLocales,
         title: 'Fitness App',
         debugShowCheckedModeBanner: false, // debug banner remove: false
         routes: {
